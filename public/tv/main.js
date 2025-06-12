01a0ba9dcdaa0990f05fea0d828e4b31f943b5f5
EventosGDN <eventos@gmail.com>
2025-06-12 01:28:41 -0300

    vemos que pasa 5

--- public/tv/main.js
+++ public/tv/main.js
@@ -1,4 +1,4 @@
-var supabase = Supabase.createClient(
+var supabase = supabase.createClient(
   'https://wqrkkkqmbrksleagqsli.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indxcmtra3FtYnJrc2xlYWdxc2xpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNTA1OTMsImV4cCI6MjA2NDYyNjU5M30.XNGR57FM29Zxskyzb8xeXLrBtH0cnco9yh5X8Sb4ISY'
 )
