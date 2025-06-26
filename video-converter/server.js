762a8fa31b381eb62e933287159792e31eead2b1
EventosGDN <eventos@gmail.com>
2025-06-26 03:40:09 -0300

    Fix: aplica headers CORS globales manualmente

--- video-converter/server.js
+++ video-converter/server.js
@@ -23,8 +23,12 @@ const corsOptions = {
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
 }
-app.use(cors(corsOptions))
-app.options('*', cors(corsOptions))
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://subilovos.vercel.app')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 
 // Multer
 const storage = multer.diskStorage({
